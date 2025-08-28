976aca99546ad0b68cff5e2a0532113028babf97
Max Groenlund <max@synthmax.dk>
2025-08-28 15:57:14 +0200

    chore(phpstan): add minimal config

---
+++ sdk/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sweetdate/sdk',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '363808efc754ca37db291ab7c903a4735ffb239b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sweetdate/sdk' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '363808efc754ca37db291ab7c903a4735ffb239b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
